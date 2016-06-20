2587ac9e232a1d751113fb10f81c21b6ed08a9e8
sevinus <denoil.song@gmail.com>
2016-06-21 00:04:32 +0900

    1. Bullet 생성 (Trail) 2. 캐릭터 애니메이션 동기화

---
+++ Assets/02.Scripts/Bullet/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+    private float m_lifeTime;
+    private Rigidbody m_cachedRigidbody;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_lifeTime = 0.5f;
+
+        m_cachedRigidbody = this.gameObject.GetComponent<Rigidbody>();
+        m_cachedRigidbody.velocity = this.gameObject.transform.forward * 10.0f;
+
+        Destroy(this.gameObject, m_lifeTime);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {	
+	}
+}
--- Assets/02.Scripts/Player/PlayerController.cs
+++ Assets/02.Scripts/Player/PlayerController.cs
@@ -4,17 +4,37 @@ using UnityStandardAssets.Utility;
 
 public class PlayerController : MonoBehaviour {
 
+    public enum AnimState
+    {
+        idle = 0,
+        runForward,
+        runBackward,
+        runLeft,
+        runRight,
+    }
+
     private Transform m_cachedTransform = null;
     private NetworkView m_networkView = null;
 
     private Vector3 m_curPosition = Vector3.zero;
     private Quaternion m_curRotation = Quaternion.identity;
 
+    private CharacterController m_charController = null;
+    private Animation m_animation = null;
+
+    public GameObject m_bullet = null;
+    public Transform m_firePos = null;
+
+    public AnimState m_animState = AnimState.idle;
+    public AnimationClip[] m_animClips;
+
     // Use this for initialization
     void Awake ()
     {
         m_cachedTransform = this.gameObject.GetComponent<Transform>();        
         m_networkView = this.gameObject.GetComponent<NetworkView>();
+        m_charController = this.gameObject.GetComponent<CharacterController>();
+        m_animation = this.gameObject.GetComponentInChildren<Animation>();
 
         m_networkView.observed = this;
 
@@ -29,13 +49,53 @@ public class PlayerController : MonoBehaviour {
 	void Update ()
     {
         if (m_networkView.isMine == true)
-            return;
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Fire();
 
-        Vector3 pos = m_cachedTransform.position;
-        m_cachedTransform.position = Vector3.Lerp(pos, m_curPosition, Time.deltaTime * 5.0f);
+                m_networkView.RPC("Fire", RPCMode.Others);
+            }
 
-        Quaternion rotation = m_cachedTransform.rotation;
-        m_cachedTransform.rotation = Quaternion.Lerp(rotation, m_curRotation, Time.deltaTime * 5.0f);
+            Vector3 localVelocity = m_cachedTransform.InverseTransformDirection(m_charController.velocity);
+            Vector3 fowardDir = new Vector3(0.0f, 0.0f, localVelocity.z);
+            Vector3 rightDir = new Vector3(localVelocity.x, 0.0f, 0.0f);
+
+            if (fowardDir.z > 0.1f)
+            {
+                m_animState = AnimState.runForward;
+            }
+            else if (fowardDir.z < -0.1f)
+            {
+                m_animState = AnimState.runBackward;
+            }
+            else if (rightDir.x < -0.1f)
+            {
+                m_animState = AnimState.runLeft;
+            }
+            else if (rightDir.x > 0.1f)
+            {
+                m_animState = AnimState.runRight;
+            }
+            else
+            {
+                m_animState = AnimState.idle;
+            }
+
+            AnimationClip clip = m_animClips[(int)m_animState];
+            m_animation.CrossFade(clip.name, 0.2f);
+        }
+        else
+        {
+            Vector3 pos = m_cachedTransform.position;
+            m_cachedTransform.position = Vector3.Lerp(pos, m_curPosition, Time.deltaTime * 5.0f);
+
+            Quaternion rotation = m_cachedTransform.rotation;
+            m_cachedTransform.rotation = Quaternion.Lerp(rotation, m_curRotation, Time.deltaTime * 5.0f);
+
+            AnimationClip clip = m_animClips[(int)m_animState];
+            m_animation.CrossFade(clip.name, 0.2f);
+        }
     }
 
     void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
@@ -44,20 +104,43 @@ public class PlayerController : MonoBehaviour {
         {
             Vector3 pos = m_cachedTransform.position;
             Quaternion rotation = m_cachedTransform.rotation;
+            int animState = (int)m_animState;
 
             stream.Serialize(ref pos);
             stream.Serialize(ref rotation);
+            stream.Serialize(ref animState);
         }
         else
         {
             Vector3 pos = Vector3.zero;
             Quaternion rotation = Quaternion.identity;
+            int animState = 0;
 
             stream.Serialize(ref pos);
             stream.Serialize(ref rotation);
+            stream.Serialize(ref animState);
 
             m_curPosition = pos;
             m_curRotation = rotation;
+            m_animState = (AnimState)animState;
+        }
+    }
+
+    [RPC]
+    void Fire()
+    {
+        if (m_bullet == null ||
+            m_firePos == null)
+            return;
+
+        GameObject.Instantiate(m_bullet, m_firePos.position, m_firePos.rotation);      
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Bullet")
+        {
+            Destroy(collider.gameObject);
         }
     }
 }
